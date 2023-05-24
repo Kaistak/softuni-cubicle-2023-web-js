cff64a03d9850cb91cf5487451965620200083d4
Kaistak <134406098+Kaistak@users.noreply.github.com>
2023-05-24 13:58:48 +0300

    Add handlebars to project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const handlebars = require('express-handlebars')
 
 const app = express();
 
 const PORT = 5000;
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+    
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', 'src/views')
+
 app.get('/', (req, res) => {
-    res.send('hello from express server')
+    res.render('index')
 })
 
 app.listen(PORT, () => console.log(`server is listennig on port ${PORT}...`))
